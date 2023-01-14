fef413599e1ca578c52691ac98829b6375a1d1d4
a-7db <Ahmed97874@gmail.com>
2023-01-14 23:30:14 +0300

    First Uploading

---
+++ razza_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
