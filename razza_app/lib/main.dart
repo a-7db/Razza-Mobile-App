093d3386fb0aa8daa3a481bf5ee510e5abefa40e
a-7db <Ahmed97874@gmail.com>
2023-01-15 16:12:34 +0300

    create welcome & login page

--- razza_app/lib/main.dart
+++ razza_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:razza_app/welcomPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: WelcomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
